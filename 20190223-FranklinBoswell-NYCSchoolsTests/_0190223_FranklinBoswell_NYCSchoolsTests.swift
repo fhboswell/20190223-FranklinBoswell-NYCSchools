e1712b5eba294e0e2f2fdb8ef5f0708c5820ed98
Henry Boswell <henry.boswell.external@porsche.digital>
2019-02-23 10:59:08 -0800

    Initial Commit

---
+++ 20190223-FranklinBoswell-NYCSchoolsTests/_0190223_FranklinBoswell_NYCSchoolsTests.swift
@@ -0,0 +1,34 @@
+//
+//  _0190223_FranklinBoswell_NYCSchoolsTests.swift
+//  20190223-FranklinBoswell-NYCSchoolsTests
+//
+//  Created by Henry Boswell on 2/23/19.
+//  Copyright © 2019 Henry Boswell. All rights reserved.
+//
+
+import XCTest
+@testable import _0190223_FranklinBoswell_NYCSchools
+
+class _0190223_FranklinBoswell_NYCSchoolsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
