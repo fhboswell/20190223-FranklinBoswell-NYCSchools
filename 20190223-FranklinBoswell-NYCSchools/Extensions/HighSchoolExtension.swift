cd6490b25e390123b5081cb33cf3e89884ca62bf
Henry Boswell <henry.boswell.external@porsche.digital>
2019-02-23 15:29:05 -0800

    initial commit

---
+++ 20190223-FranklinBoswell-NYCSchools/Extensions/HighSchoolExtension.swift
@@ -0,0 +1,9 @@
+//
+//  HighSchoolExtension.swift
+//  20190223-FranklinBoswell-NYCSchools
+//
+//  Created by Henry Boswell on 2/23/19.
+//  Copyright © 2019 Henry Boswell. All rights reserved.
+//
+
+import Foundation
---
+++ 20190223-FranklinBoswell-NYCSchools/Models/HighSchool.swift
@@ -0,0 +1,17 @@
+//
+//  HighschoolData.swift
+//  20190223-FranklinBoswell-NYCSchools
+//
+//  Created by Henry Boswell on 2/23/19.
+//  Copyright © 2019 Henry Boswell. All rights reserved.
+//
+
+import Foundation
+
+struct HighSchool {
+    
+    let dbn: String
+    let name: String
+    let location: (latitude: Double, longitude: Double)
+    
+}
